adce0eff992496b62f4cf7949e6b646e94efe14e
HOkket <76225268+HOkket@users.noreply.github.com>
2023-07-05 17:31:10 -0300

    Função

--- Gerador_de_senha.py
+++ Gerador_de_senha.py
@@ -1,24 +1,51 @@
 #!/bin/python3
 import random
 
-print("1 - Gerador aleatorio de senhas")
-print("2 - Gerador de senhas direcionado")
-MODO = int(input("Escolha o modo de gerarar suas senha: "))
 
-if MODO == 1:
-    tamanho = int(input("Tamanho da senha: "))
-    ALFA = "QWERTYUIOPASDFGHJKLÇZXCVBNM"
-    alfa = "qwertyuiopasdfghjklçzxcvbnm"
-    numeros = "1234567890"
-    especial = "!@#$%¨&*()?/}]{[+="
-    Estrutura = ALFA + alfa + numeros + especial
-    senha = "".join(random.sample(Estrutura,tamanho))
-    print(senha)
-elif MODO == 2:
-    tamanho = int(input("Tamanho da senha: "))
-    PRIMEIRA_CHAVE = str(input("Isira a primeira palavra chave: "))
-    SEGUNDA_CHAVE = str(input("Isira a segunda palavra chave: "))
-    numeros = input("Insira os caracteres especiais ou numeros (opcional): ")
-    Estrutura = PRIMEIRA_CHAVE + SEGUNDA_CHAVE + numeros
-    senha = "".join(random.sample(Estrutura,tamanho))
-    print(senha)
+def senha_aleatoria():
+    if __name__ == "__main__":
+        tamanho = int(input("Tamanho da senha: "))
+        ALFA = "QWERTYUIOPASDFGHJKLÇZXCVBNM"
+        alfa = "qwertyuiopasdfghjklçzxcvbnm"
+        numeros = "1234567890"
+        especial = "!@#$%¨&*()?/}]{[+="
+        Estrutura = ALFA + alfa + numeros + especial
+        senha = "".join(random.sample(Estrutura,tamanho))
+        return(senha)
+
+
+
+
+def senha_direcionada():
+    if __name__ == "__main__":
+        tamanho = int(input("Tamanho da senha: "))
+        PRIMEIRA_CHAVE = str(input("Isira a primeira palavra chave: "))
+        SEGUNDA_CHAVE = str(input("Isira a segunda palavra chave: "))
+        numeros = input("Insira os caracteres especiais ou numeros (opcional): ")
+        Estrutura = PRIMEIRA_CHAVE + SEGUNDA_CHAVE + numeros
+        senha = "".join(random.sample(Estrutura,tamanho))
+        return(senha)
+
+
+
+
+def gerador_wordlist():
+    if __name__ == "__main__":
+        tamanho = int(input("Tamanho da senha: "))
+        quantidade = int(input("Quantas senhas você deseja gerar? "))
+        arquivo = open('wordlist.txt', 'a')  # Abrir o arquivo no modo de adição
+        contagem = 0
+        for i in range(quantidade):
+            ALFA = "QWERTYUIOPASDFGHJKLÇZXCVBNM"
+            alfa = "qwertyuiopasdfghjklçzxcvbnm"
+            numeros = "1234567890"
+            especial = "!@#$%¨&*()?/}]{[+="
+            Estrutura = ALFA + alfa + numeros + especial
+            senha = "".join(random.sample(Estrutura, tamanho))
+            print(senha, file=arquivo)  # Salvar a senha no arquivo
+            i += 1
+            contagem += 1
+            porcentagem = (contagem / quantidade) * 100
+            print("(", porcentagem, "%", ")")
+            arquivo.close()  # Fechar o arquivo fora do loop
+            print("Finalizado")
