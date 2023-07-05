8b4cf90b56f5af7ded505a3b085a5680f5bd401e
HOkket <76225268+HOkket@users.noreply.github.com>
2023-07-05 17:40:32 -0300

    Ordenação

--- Gerador_de_senha.py
+++ Gerador_de_senha.py
@@ -1,51 +1,44 @@
-#!/bin/python3
 import random
 
-
 def senha_aleatoria():
-    if __name__ == "__main__":
-        tamanho = int(input("Tamanho da senha: "))
+    tamanho = int(input("Tamanho da senha: "))
+    ALFA = "QWERTYUIOPASDFGHJKLÇZXCVBNM"
+    alfa = "qwertyuiopasdfghjklçzxcvbnm"
+    numeros = "1234567890"
+    especial = "!@#$%¨&*()?/}]{[+="
+    Estrutura = ALFA + alfa + numeros + especial
+    senha = "".join(random.sample(Estrutura, tamanho))
+    return senha
+
+def senha_direcionada():
+    tamanho = int(input("Tamanho da senha: "))
+    PRIMEIRA_CHAVE = input("Insira a primeira palavra chave: ")
+    SEGUNDA_CHAVE = input("Insira a segunda palavra chave: ")
+    numeros = input("Insira os caracteres especiais ou números (opcional): ")
+    Estrutura = PRIMEIRA_CHAVE + SEGUNDA_CHAVE + numeros
+    senha = "".join(random.sample(Estrutura, tamanho))
+    return senha
+
+def gerador_wordlist():
+    tamanho = int(input("Tamanho da senha: "))
+    quantidade = int(input("Quantas senhas você deseja gerar? "))
+    arquivo = open('wordlist.txt', 'a')  # Abrir o arquivo no modo de adição
+    contagem = 0
+    for i in range(quantidade):
         ALFA = "QWERTYUIOPASDFGHJKLÇZXCVBNM"
         alfa = "qwertyuiopasdfghjklçzxcvbnm"
         numeros = "1234567890"
         especial = "!@#$%¨&*()?/}]{[+="
         Estrutura = ALFA + alfa + numeros + especial
-        senha = "".join(random.sample(Estrutura,tamanho))
-        return(senha)
+        senha = "".join(random.sample(Estrutura, tamanho))
+        print(senha, file=arquivo)  # Salvar a senha no arquivo
+        contagem += 1
+        porcentagem = (contagem / quantidade) * 100
+        print("({:.2f}%)".format(porcentagem))
+    arquivo.close()  # Fechar o arquivo fora do loop
+    print("Finalizado")
 
 
-
-
-def senha_direcionada():
-    if __name__ == "__main__":
-        tamanho = int(input("Tamanho da senha: "))
-        PRIMEIRA_CHAVE = str(input("Isira a primeira palavra chave: "))
-        SEGUNDA_CHAVE = str(input("Isira a segunda palavra chave: "))
-        numeros = input("Insira os caracteres especiais ou numeros (opcional): ")
-        Estrutura = PRIMEIRA_CHAVE + SEGUNDA_CHAVE + numeros
-        senha = "".join(random.sample(Estrutura,tamanho))
-        return(senha)
-
-
-
-
-def gerador_wordlist():
-    if __name__ == "__main__":
-        tamanho = int(input("Tamanho da senha: "))
-        quantidade = int(input("Quantas senhas você deseja gerar? "))
-        arquivo = open('wordlist.txt', 'a')  # Abrir o arquivo no modo de adição
-        contagem = 0
-        for i in range(quantidade):
-            ALFA = "QWERTYUIOPASDFGHJKLÇZXCVBNM"
-            alfa = "qwertyuiopasdfghjklçzxcvbnm"
-            numeros = "1234567890"
-            especial = "!@#$%¨&*()?/}]{[+="
-            Estrutura = ALFA + alfa + numeros + especial
-            senha = "".join(random.sample(Estrutura, tamanho))
-            print(senha, file=arquivo)  # Salvar a senha no arquivo
-            i += 1
-            contagem += 1
-            porcentagem = (contagem / quantidade) * 100
-            print("(", porcentagem, "%", ")")
-            arquivo.close()  # Fechar o arquivo fora do loop
-            print("Finalizado")
+print(senha_aleatoria())
+print(senha_direcionada())
+gerador_wordlist()
