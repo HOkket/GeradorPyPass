fa18eae8828075a5a32dd73b8ed8f72474a1232d
HOkket <76225268+HOkket@users.noreply.github.com>
2023-07-05 17:48:42 -0300

    teste

--- menu.py
+++ menu.py
@@ -3,12 +3,13 @@ import importlib
 import Gerador_de_senha
 
 
-print("[1]Gerador de senhas aleatorias")
-print("[2]Gerador de senhas codicionais")
+print("[1] Gerador de senhas aleatorias")
+print("[2] Gerador de senhas codicionais")
+print("[3] Gerador de wordlist")
 opcao = int(input())
 
 if opcao == 1:
-    modulo = importlib.senha_aleatoria('senhas_aleatoria')
+    modulo = importlib.import_module('senhas_aleatoria')
     modulo.executar()
 elif opcao == 2:
     modulo = importlib.import_module('senha_direcionada')
